927ffa6df6b23c6862e851709f56ececd565082b
Eugene Kuz <tir26@ukr.net>
2024-10-07 10:55:31 +0300

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,19 @@
 import styles from "./Contact.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Contact = ({ contact: { id, name, number }, onDelete }) => {
   return (
     <>
       <div className={styles.contactInfo}>
-        <p className={styles.contactContent}>{name}</p>
-        <p className={styles.contactContent}>{number}</p>
+        <p className={styles.contactContent}>
+          <IoMdPerson className={styles.icon} />
+          {name}
+        </p>
+        <p className={styles.contactContent}>
+          <FaPhoneAlt className={styles.icon} />
+          {number}
+        </p>
       </div>
       <button
         type="button"
